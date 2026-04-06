25e80b6eb71412402205c2d0e3c2a777eb79b326
Olig Dev <beatsolig@gmail.com>
2026-04-06 08:09:52 -0300

    Update 11.py
    Added a comment to explain the functionality of the code.

--- atividade_3/11.py
+++ atividade_3/11.py
@@ -1,3 +1,4 @@
+#Leia um número e: Se for par e positivo → “Par positivo”; Se for par e negativo → “Par negativo”; Caso contrário → “Ímpar”.
 num = int(input("Digite um número: "))
 if num % 2 == 0 and num > 0:
     print("Par positivo")
