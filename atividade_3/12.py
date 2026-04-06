14394a1c7d8ea9f8783c888da97828bba9290cb4
Olig Dev <beatsolig@gmail.com>
2026-04-06 08:10:48 -0300

    Update 12.py
    Added a comment at the beginning to describe the program's functionality.

--- atividade_3/12.py
+++ atividade_3/12.py
@@ -1,3 +1,4 @@
+#Leia dois números e: Mostre a soma; Mostre qual é maior ou se são iguais.
 num1 = float(input("Digite o primeiro número: "))
 num2 = float(input("Digite o segundo número: "))
 soma = num1 + num2
