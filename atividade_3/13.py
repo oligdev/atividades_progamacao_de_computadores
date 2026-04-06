770227211d3576fabfa677ef2a10f31e7f2df3cb
Olig Dev <beatsolig@gmail.com>
2026-04-06 08:11:20 -0300

    Update 13.py
    Updated logic to display double of the number when less than or equal to 100.

--- atividade_3/13.py
+++ atividade_3/13.py
@@ -1,3 +1,4 @@
+#Leia um número e: Se for maior que 100 → mostre metade; Senão → mostre o dobro.
 num = float(input("Digite um número: "))
 if num > 100:
     print("A metade do número é:", num / 2)
