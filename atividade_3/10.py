d0664b7b4aa1ec31bdf35c331328b162152001ef
Olig Dev <beatsolig@gmail.com>
2026-04-06 08:09:10 -0300

    Refactor number range check for user input
    Updated input handling to provide feedback based on the number range.

--- atividade_3/10.py
+++ atividade_3/10.py
@@ -1,3 +1,4 @@
+#Leia um número e informe: “Dentro do intervalo” se estiver entre 0 e 10; “Fora do intervalo” caso contrário.
 num = float(input("Digite um número: "))
 if 0 <= num <= 10:
     print("Dentro do intervalo")
