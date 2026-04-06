958243efece4fcbce0945ddb38dde90bdc30242a
Olig Dev <beatsolig@gmail.com>
2026-04-06 08:12:33 -0300

    Update 14.py
    Refactor input handling and output messages for clarity.

--- atividade_3/14.py
+++ atividade_3/14.py
@@ -1,3 +1,4 @@
+#Leia um valor e: Converta para inteiro; Se for múltiplo de 3 → “Múltiplo de 3”; Senão → “Não é múltiplo”.
 num = int(input("Digite um número: "))
 if num % 3 == 0:
     print("Múltiplo de 3")
